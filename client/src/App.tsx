91bd72ea8d932a68c23747966a481cdb16b90c4d
ckwong <chunkang.wong94@gmail.com>
2023-09-27 23:22:02 +0800

    Added loading state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,22 +5,44 @@ import { Post } from "./models/Post.model";
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [isFetchingPosts, setIsFetchingPosts] = useState(false);
+  const [fetchPostError, setFetchPostError] = useState<Error | null>(null);
+
+  const [isCreatingPost, setIsCreatingPost] = useState(false);
+  const [createPostError, setCreatePostError] = useState<Error | null>(null);
 
   const [title, setTitle] = useState("");
 
   useEffect(() => {
     const init = async () => {
-      const newPosts = await getPosts();
-      setPosts(newPosts);
+      try {
+        setIsFetchingPosts(true);
+        const newPosts = await getPosts();
+        setPosts(newPosts);
+        setFetchPostError(null);
+      } catch (error) {
+        setFetchPostError(error as Error);
+      } finally {
+        setIsFetchingPosts(false);
+      }
     };
     init();
   }, []);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const newPost = await createPost(title);
-    setTitle("");
-    setPosts([newPost, ...posts]);
+    try {
+      setIsCreatingPost(true);
+      const newPost = await createPost(title);
+      setPosts([newPost, ...posts]);
+      setCreatePostError(null);
+    } catch (error) {
+      setCreatePostError(error as Error);
+      console.error(error);
+    } finally {
+      setIsCreatingPost(false);
+      setTitle("");
+    }
   };
 
   return (
@@ -34,9 +56,25 @@ function App() {
           autoFocus
           onChange={(e) => setTitle(e.target.value)}
           required
+          disabled={isCreatingPost}
         />
-        <button type="submit">Add</button>
+        <button type="submit" disabled={isCreatingPost}>
+          {isCreatingPost ? "Creating..." : "Create"}
+        </button>
+        {createPostError ? (
+          <p style={{ color: "red" }}>{createPostError.message}</p>
+        ) : null}
       </form>
+      {isFetchingPosts ? <p>Loading...</p> : null}
+      {fetchPostError ? (
+        <p
+          style={{
+            color: "red",
+          }}
+        >
+          {fetchPostError.message}
+        </p>
+      ) : null}
       <ul>
         {posts.map((post) => {
           return <li key={post.id}>{post.title}</li>;
