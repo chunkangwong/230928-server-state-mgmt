631cc9685e238090c1b6a8d97ee292505e5fc037
ckwong <chunkang.wong94@gmail.com>
2023-09-28 00:00:44 +0800

    Added loading state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { createPost, getPosts } from "./controllers/posts.controller";
+import { getPosts } from "./controllers/posts.controller";
 import { Post } from "./models/Post.model";
 
 function App() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [isFetchingPosts, setIsFetchingPosts] = useState(false);
   const [fetchPostError, setFetchPostError] = useState<Error | null>(null);
-
-  const [isCreatingPost, setIsCreatingPost] = useState(false);
-  const [createPostError, setCreatePostError] = useState<Error | null>(null);
-
-  const [title, setTitle] = useState("");
 
   useEffect(() => {
     const init = async () => {
@@ -29,42 +24,8 @@ function App() {
     init();
   }, []);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      setIsCreatingPost(true);
-      const newPost = await createPost(title);
-      setPosts([newPost, ...posts]);
-      setCreatePostError(null);
-    } catch (error) {
-      setCreatePostError(error as Error);
-      console.error(error);
-    } finally {
-      setIsCreatingPost(false);
-      setTitle("");
-    }
-  };
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="title"
-          type="text"
-          placeholder="Add new post"
-          value={title}
-          autoFocus
-          onChange={(e) => setTitle(e.target.value)}
-          required
-          disabled={isCreatingPost}
-        />
-        <button type="submit" disabled={isCreatingPost}>
-          {isCreatingPost ? "Creating..." : "Create"}
-        </button>
-        {createPostError ? (
-          <p style={{ color: "red" }}>{createPostError.message}</p>
-        ) : null}
-      </form>
       {isFetchingPosts ? <p>Loading...</p> : null}
       {fetchPostError ? (
         <p
